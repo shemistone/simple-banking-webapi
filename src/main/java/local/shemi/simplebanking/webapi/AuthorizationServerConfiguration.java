43cf1fcd961fb1a44d3f0cae652bda5ceb311b6b
shemistone <shemistne@gmail.com>
2020-11-13 14:40:21 +0300

    Add OAUTH Configuration (Client Credentials Grant Type)
    After this commit, the application will be able to issue access tokens 
    to clients using the Client Credentials Grant Type. In order for clients
    to access any resource, they will need to supply the issued access token.

---
+++ src/main/java/local/shemi/simplebanking/webapi/AuthorizationServerConfiguration.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local.shemi.simplebanking.webapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import local.shemi.simplebanking.webapi.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+
+/**
+ *
+ * @author bmg
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory()
+//                .withClient("client_a")
+//                .secret(passwordEncoder().encode("password_a"))
+//                .authorities("ROLE_A")
+//                .scopes("all")
+//                .authorizedGrantTypes("client_credentials")
+//                .accessTokenValiditySeconds(120)
+//                .refreshTokenValiditySeconds(600)
+//                .and()
+//                .withClient("client_b")
+//                .secret(passwordEncoder().encode("password_b"))
+//                .authorities("ROLE_B")
+//                .scopes("all")
+//                .authorizedGrantTypes("client_credentials")
+//                .accessTokenValiditySeconds(120)
+//                .refreshTokenValiditySeconds(600);
+        clients.withClientDetails((String clientId) -> {
+            Client client = new Client(clientId, passwordEncoder().encode("password_a"));
+            if (client == null) {
+                throw new ClientRegistrationException("Client not found: " + clientId);
+            }
+            BaseClientDetails clientDetails = new BaseClientDetails();
+            clientDetails.setClientId(clientId);
+            clientDetails.setClientSecret(client.getPassword());
+            clientDetails.setAuthorizedGrantTypes(List.of("client_credentials"));
+            clientDetails.setScope(List.of("all"));
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_A"));
+            clientDetails.setAuthorities(authorities);
+            clientDetails.setAccessTokenValiditySeconds(120);
+            clientDetails.setRefreshTokenValiditySeconds(600);
+            return clientDetails;
+        });
+//        clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .setClientDetailsService(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/local/shemi/simplebanking/webapi/ResourceServerConfiguration.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local.shemi.simplebanking.webapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+/**
+ *
+ * @author bmg
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("simple-banking-webapi").stateless(false);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.anonymous().disable()
+                .authorizeRequests().antMatchers("/customers/**").authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+
+    }
+
+}
